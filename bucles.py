0a10913a85cda2138ccc0f1555d3105f4001d74b
Lizeth-Cruz18 <leon182015@gmail.com>
2025-05-23 21:04:04 -0500

    version2

---
+++ bucles.py
@@ -0,0 +1,4 @@
+
+frutas = ["manzana", "plátano", "guayaba"]
+for fruta in frutas:
+    print(f"Me gusta la {fruta}")
--- condicionales.py
+++ condicionales.py
@@ -1,1 +1,9 @@
 print("ejemplos de condicionales")
+
+usuario_activo = True
+rol = "admin"
+
+if usuario_activo and rol in ["admin", "supervisor"]:
+    print("Acceso completo")
+else:
+    print("Acceso limitado")
